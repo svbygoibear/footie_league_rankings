168bad7438acf5099039f794cd946cea45dc82d9
Simone van Buuren <svbuuren@fullfacing.com>
2016-10-28 23:19:13 +0200

    Finished file interpreter which generates matches and team from lines of string.

--- src/main/scala/framework/FileInterpreter.scala
+++ src/main/scala/framework/FileInterpreter.scala
@@ -1,13 +1,34 @@
 package framework
 
+import scala.annotation.tailrec
+import scala.util.matching.Regex
+import scalaz._
+import Scalaz._
+import scala.language.postfixOps
+
 /**
   * Created by Simone van Buuren on 2016/10/28.
+  * Usage: Used to interpret lines of a file to generate usable case classes from the data
   */
 
-class FileInterpreter {
+object FileInterpreter {
   val regex = "([\\w\\s]+[\\D])([\\d]+)".r
 
-  def createMatch() = {}
+  // Generates a match from a line of string
+  def createMatch(line: String): Match = {
+    val teams = line.split(',') toList
 
-  def createTeam() = {}
+    @tailrec
+    def loop(teams: List[String], tSeq: Seq[Team]): Match = teams match {
+      case nil => Match(tSeq)
+      case head => loop(teams.tail, tSeq :+ createTeam(teams.head))
+    }
+    loop(teams, Seq())
+  }
+
+  // Creates a team object from a string
+  def createTeam(section: String): Team = {
+    val regex(name, goals) = section
+    Team(name, goals.toInt)
+  }
 }
